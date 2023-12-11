5b4be002bb775b7d7edc85cbc5ff305f1467cdcf
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-12-11 02:21:24 -0500

    added particles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <Logo /> 
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+		<div className='App'>
+			<ParticlesBg type='cobweb' num={99} bg={true} />
+			<Navigation />
+			<Logo />
+			<Rank />
+			<ImageLinkForm />
+			{/* <FaceRecognition /> */}
+		</div>
+	);
 }
 
 export default App;
