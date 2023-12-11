c8162b3a6c56fb5c96a1e3c99fdb14ff52921c4e
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-12-11 01:13:55 -0500

    fixed the background and increased the max tilt from looking at documentation and removed the div that was making the tilt ref the full vw

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
+import brain from './brain.png'
 import './Logo.css';
 
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
-			<Tilt>
-				<div className='Tilt br2 shadow-2' style={{ height: '150px', width: '150px' }}>
-					<h1>React Parallax Tilt 👀</h1>
-				</div>
+			<Tilt className='Tilt br2 shadow-2' tiltMaxAngleX={50} tiltMaxAngleY={50} style={{ height: '150px', width: '150px' }}>
+                <div className='Tilt-inner pa3'>
+                    <img style={{paddingTop: '5px'}} alt='logo' src={brain}></img>
+                </div>
 			</Tilt>
 		</div>
 	);
