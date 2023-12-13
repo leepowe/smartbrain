132b2546a106b3e2d9b9d099f7690bf30406bd7e
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-12-13 00:03:56 -0500

    set up the api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
 import React, { Component } from 'react';
 import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai'
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-function App() {
-  return (
-		<div className='App'>
-			<ParticlesBg type='cobweb' num={99} bg={true} />
-			<Navigation />
-			<Logo />
-			<Rank />
-			<ImageLinkForm />
-			{/* <FaceRecognition /> */}
-		</div>
-	);
+const app = new Clarifai.App({
+	apiKey: '4efce0f807a243bca3379fe94b20970c'
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onButtonSubmit = () => {
+		this.setState({ imageUrl: this.state.input });
+
+		app.models
+			.predict('face-detection', this.state.input)
+			.then((response) => {
+				console.log('hi', response);
+				if (response) {
+					fetch('http://localhost:3000/image', {
+						method: 'put',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({
+							id: this.state.user.id,
+						}),
+					})
+						.then((response) => response.json())
+						.then((count) => {
+							this.setState(Object.assign(this.state.user, { entries: count }));
+						});
+				}
+				this.displayFaceBox(this.calculateFaceLocation(response));
+			})
+			.catch((err) => console.log(err));
+	}
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <ParticlesBg type='cobweb' num={99} bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} 
+        />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import './Logo.css';
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
-			<Tilt className='Tilt br2 shadow-2' tiltMaxAngleX={50} tiltMaxAngleY={50} style={{ height: '150px', width: '150px' }}>
+			<Tilt className='Tilt br2 shadow-2' tiltMaxAngleX={-30} tiltMaxAngleY={-15} style={{ height: '150px', width: '150px' }}>
                 <div className='Tilt-inner pa3'>
                     <img style={{paddingTop: '5px'}} alt='logo' src={brain}></img>
                 </div>
