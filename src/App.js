65345860110c9a0dfedde8a03470922d57770724
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-12-17 22:02:11 -0500

    and... done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ParticlesBg from 'particles-bg';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -19,105 +21,116 @@ class App extends Component {
 			input: '',
 			imageUrl: '',
 			box: {},
-			// route: 'signin',
-			// isSignedIn: false,
-			// user: {
-			// 	id: '',
-			// 	name: '',
-			// 	email: '',
-			// 	entries: 0,
-			// 	joined: ''
-			// }
+			route: 'signin',
+			isSignedIn: false,
+			user: {
+				id: '',
+				name: '',
+				email: '',
+				entries: 0,
+				joined: '',
+			},
 		};
 	}
 
-	// loadUser = (data) => {
-	// 	this.setState({
-	// 		user: {
-	// 			id: data.id,
-	// 			name: data.name,
-	// 			email: data.email,
-	// 			entries: data.entries,
-	// 			joined: data.joined,
-	// 		},
-	// 	});
-	// };
+	loadUser = (data) => {
+		this.setState({
+			user: {
+				id: data.id,
+				name: data.name,
+				email: data.email,
+				entries: data.entries,
+				joined: data.joined,
+			},
+		});
+	};
 
 	calculateFaceLocation = (data) => {
 		const clarifaiFace =
 			data.outputs[0].data.regions[0].region_info.bounding_box;
-		const image = document.getElementById('input-image');
+		const image = document.getElementById('inputimage');
 		const width = Number(image.width);
 		const height = Number(image.height);
 		return {
 			leftCol: clarifaiFace.left_col * width,
 			topRow: clarifaiFace.top_row * height,
-			rightCol: width - (clarifaiFace.right_col * width),
-			bottomRow: height - (clarifaiFace.bottom_row * height),
-		}
-	}
+			rightCol: width - clarifaiFace.right_col * width,
+			bottomRow: height - clarifaiFace.bottom_row * height,
+		};
+	};
 
 	displayFaceBox = (box) => {
-		console.log(box);
-		this.setState({ box })
-	}
+		this.setState({ box: box });
+	};
 
 	onInputChange = (event) => {
-		this.setState({ input: event.target.value })
-	}
+		this.setState({ input: event.target.value });
+	};
 
 	onButtonSubmit = () => {
 		this.setState({ imageUrl: this.state.input });
+
 		app.models
-			.predict(
-				Clarifai.FACE_DETECT_MODEL,
-				this.state.input)
-			.then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-			.catch(err => console.log(err));		
+			.predict('face-detection', this.state.input)
+			.then((response) => {
+				console.log('hi', response);
+				if (response) {
+					fetch('http://localhost:3000/image', {
+						method: 'put',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({
+							id: this.state.user.id,
+						}),
+					})
+						.then((response) => response.json())
+						.then((count) => {
+							this.setState(Object.assign(this.state.user, { entries: count }));
+						});
+				}
+				this.displayFaceBox(this.calculateFaceLocation(response));
+			})
+			.catch((err) => console.log(err));
 	};
 
-	// onRouteChange = (route) => {
-	// 	if (route === 'signout') {
-	// 		this.setState({ isSignedIn: false });
-	// 	} else if (route === 'home') {
-	// 		this.setState({ isSignedIn: true });
-	// 	}
-	// 	this.setState({ route: route });
-	// };
+	onRouteChange = (route) => {
+		if (route === 'signout') {
+			this.setState({ isSignedIn: false });
+		} else if (route === 'home') {
+			this.setState({ isSignedIn: true });
+		}
+		this.setState({ route: route });
+	};
 
 	render() {
+		const { isSignedIn, imageUrl, route, box } = this.state;
 		return (
 			<div className='App'>
 				<ParticlesBg type='cobweb' bg={true} />
 				<Navigation
-				// isSignedIn={isSignedIn}
-				// onRouteChange={this.onRouteChange}
+					isSignedIn={isSignedIn}
+					onRouteChange={this.onRouteChange}
 				/>
-				{/* {route === 'home' ? ( */}
+				{route === 'home' ? (
 					<div>
 						<Logo />
 						<Rank
-						// name={this.state.user.name
-						// entries={this.state.user.entries}
+							name={this.state.user.name}
+							entries={this.state.user.entries}
 						/>
 						<ImageLinkForm
 							onInputChange={this.onInputChange}
 							onButtonSubmit={this.onButtonSubmit}
 						/>
-						<FaceRecognition
-							// imageUrl={this.state.imageUrl}
-							// box={box}
-							imageUrl={imageUrl}
-						/>
+						<FaceRecognition box={box} imageUrl={imageUrl} />
 					</div>
-				{/* ) : route === 'signing' ? (
+				) : route === 'signin' ? (
 					<Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
 				) : (
 					<Register
 						loadUser={this.loadUser}
 						onRouteChange={this.onRouteChange}
 					/>
-				)} */}
+				)}
 			</div>
 		);
 	}
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
 	return (
-		<div className='center pa3 ma'>
+		<div className='center ma'>
 			<div className='absolute mt2'>
-				<img id='input-image'src={imageUrl} alt='' width='500px' height='auto' />
+				<img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto' />
+				<div
+					className='bounding-box'
+					style={{
+						top: box.topRow,
+						right: box.rightCol,
+						bottom: box.bottomRow,
+						left: box.leftCol,
+					}}
+				></div>
 			</div>
 		</div>
 	);
