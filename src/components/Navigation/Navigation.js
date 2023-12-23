4e99d5b0bd502e296641708e2a0aae08241cc7bc
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-12-22 21:42:02 -0500

    took out the | from the signin register nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -21,7 +21,6 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
 				>
 					Sign In
                 </p>
-                <p>|</p>
 				<p
 					onClick={() => onRouteChange('register')}
 					className='f3 link dim black underline pa3 pointer'
